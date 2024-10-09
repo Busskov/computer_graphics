663e1c1e0ada015bc3ee13a64eed956118f18a0b
Vova Busko <bus9ko@gmail.com>
2024-10-09 14:15:25 +0300

    Working version

--- lab_01/script.js
+++ lab_01/script.js
@@ -11,7 +11,8 @@ const palette = 'palette'
 const Skip = {
     "RGB": 0,
     "CMYK": 1,
-    "HSV": 2
+    "HSV": 2,
+    "PALETTE": 3
 }
 
 const Type = {
@@ -44,6 +45,8 @@ document.addEventListener('DOMContentLoaded',
         hsvRange.forEach(element => {
             document.getElementById(element).addEventListener('input', updateHsvRange)
         });
+
+        document.getElementById(palette).addEventListener('input', updatePalette)
     }
 )
 
@@ -56,9 +59,9 @@ function updateRgbRange() {
 }
 
 function updateRgb(r_id, g_id, b_id, type) {
-    r = document.getElementById(r_id).value
-    g = document.getElementById(g_id).value
-    b = document.getElementById(b_id).value
+    r = parseInt(document.getElementById(r_id).value)
+    g = parseInt(document.getElementById(g_id).value)
+    b = parseInt(document.getElementById(b_id).value)
     updateAll(r, g, b, Skip.RGB, type)
 }
 
@@ -71,14 +74,14 @@ function updateCmykRange() {
 }
 
 function updateCmyk(c_id, m_id, y_id, k_id, type) {
-    c = document.getElementById(c_id).value
-    m = document.getElementById(m_id).value
-    y = document.getElementById(y_id).value
-    k = document.getElementById(k_id).value
+    c = parseInt(document.getElementById(c_id).value)
+    m = parseInt(document.getElementById(m_id).value)
+    y = parseInt(document.getElementById(y_id).value)
+    k = parseInt(document.getElementById(k_id).value)
     r = 255 * (100 - c) * (100 - k) / 10000
     g = 255 * (100 - m) * (100 - k) / 10000
     b = 255 * (100 - y) * (100 - k) / 10000
-    updateAll(r, g, b, Skip.CMYK, type)
+    updateAll(Math.round(r), Math.round(g), Math.round(b), Skip.CMYK, type)
 }
 
 function updateHsvNumbers() {
@@ -90,30 +93,18 @@ function updateHsvRange() {
 }
 
 function updateHsv(h_id, s_id, v_id, type) {
-    h = document.getElementById(h_id).value
-    s = document.getElementById(s_id).value
-    v = document.getElementById(v_id).value
-    var r, g, b;
-
-    var i = Math.floor(h * 6);
-    var f = h * 6 - i;
-    var p = v * (1 - s);
-    var q = v * (1 - f * s);
-    var t = v * (1 - (1 - f) * s);
-
-    switch(i % 6){
-        case 0: r = v, g = t, b = p; break;
-        case 1: r = q, g = v, b = p; break;
-        case 2: r = p, g = v, b = t; break;
-        case 3: r = p, g = q, b = v; break;
-        case 4: r = t, g = p, b = v; break;
-        case 5: r = v, g = p, b = q; break;
-    }
-    updateAll(r, g, b, Skip.HSV, type)
+    h = parseInt(document.getElementById(h_id).value)
+    s = parseInt(document.getElementById(s_id).value)
+    v = parseInt(document.getElementById(v_id).value)
+    
+    rgb = hsvToRgb(h, s, v)
+    updateAll(rgb[0], rgb[1], rgb[2], Skip.HSV, type)
 }
 
 function updatePalette() {
-    color = document.getElementById(palette)
+    color = document.getElementById(palette).value
+    colors = hexToRgv(color)
+    updateAll(colors[0], colors[1], colors[2], Skip.PALETTE, 0)
 }
 
 function updateAll(r, g, b, skip, type) {
@@ -140,10 +131,17 @@ function updateAll(r, g, b, skip, type) {
         document.getElementById(rgbRange[2]).value = b
     }
     
-    k = min(100 - r * 100 / 255, 100 - g * 100 / 255, 100 - b * 100 / 255)
-    c = (100 - r * 100 / 255 - k) / (100 - k) * 100
-    m = (100 - g * 100 / 255 - k) / (100 - k) * 100
-    y = (100 - b * 100 / 255 - k) / (100 - k) * 100
+    k = Math.min(100 - r * 100 / 255, 100 - g * 100 / 255, 100 - b * 100 / 255)
+    if (k == 100) {
+        c = 0
+        m = 0
+        y = 0
+    } else {
+        c = Math.round((100 - r * 100 / 255 - k) / (100 - k) * 100)
+        m = Math.round((100 - g * 100 / 255 - k) / (100 - k) * 100)
+        y = Math.round((100 - b * 100 / 255 - k) / (100 - k) * 100)
+    }
+
     if (skip == Skip.CMYK) {
         if (type != Type.NUMBER) {
             document.getElementById(cmykNumbers[0]).value = c
@@ -168,37 +166,15 @@ function updateAll(r, g, b, skip, type) {
         document.getElementById(cmykRange[3]).value = k
     }
 
-    v = max(r / 255, g / 255, b / 255)
-    tmp = min(r / 255, g / 255, b / 255)
-    if (v == 0) {
-        s = 0
-    } else {
-        s = (v - tmp) / v
+    if (skip != Skip.PALETTE) {
+        document.getElementById(palette).value = rgbToHex(r, g, b)
     }
 
-    if (s == 0) {
-        h = 0
-    } else {
-        cr = (v - r / 255) / (v - tmp)
-        cg = (v - g / 255) / (v - tmp)
-        cb = (v - b / 255) / (v - tmp)
-    }
-
-    if (r / 255 == v) {
-        h = cb - cg
-    }
-    if (g / 255 == v) {
-        h = 2 + gr - gb
-    }
-    if (b / 255 == v) {
-        h = 4 + cg - cr
-    }
-    h = h * 60
-    if (h < 0) {
-        h = h + 360
-    }
-    v = v * 100
-    s = s * 100
+    hsl = rgbToHSL(r, g, b)
+    hsv = hslToHsv(hsl[0], hsl[1], hsl[2])
+    h = hsv[0]
+    s = hsv[1]
+    v = hsv[2]
 
     if (skip == Skip.HSV) {
         if (type != Type.NUMBER) {
@@ -221,22 +197,78 @@ function updateAll(r, g, b, skip, type) {
     }
 }
 
-function min(a, b, c) {
-    if (a <= b && a <= c) {
-        return a
+function hsvToRgb(h, s, v){
+    h /= 100
+    s /= 100
+    v /= 100
+    var r, g, b;
+
+    var i = Math.floor(h * 6);
+    var f = h * 6 - i;
+    var p = v * (1 - s);
+    var q = v * (1 - f * s);
+    var t = v * (1 - (1 - f) * s);
+
+    switch(i % 6){
+        case 0: r = v, g = t, b = p; break;
+        case 1: r = q, g = v, b = p; break;
+        case 2: r = p, g = v, b = t; break;
+        case 3: r = p, g = q, b = v; break;
+        case 4: r = t, g = p, b = v; break;
+        case 5: r = v, g = p, b = q; break;
     }
-    if (b <= a && b <= c) {
-        return b
-    }
-    return c
+
+    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
 }
 
-function max(a, b, c) {
-    if (a >= b && a >= c) {
-        return a
+function rgbToHSL(r, g, b) {
+    r /= 255
+    g /= 255
+    b /= 255
+    let max = Math.max(r, g, b), min = Math.min(r, g, b)
+    let h, s, l = (max + min) / 2;
+    if (max == min) {
+        h = s = 0;
+    } else {
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch (max) {
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+
+        h /= 6;
     }
-    if (b >= a && b >= c) {
-        return b
+
+    h = Math.round(h * 360);
+    s = Math.round(s * 100);
+    l = Math.round(l * 100);
+    return [h, s, l]
+}
+
+function hslToHsv(h, s, l) {
+    s /= 100
+    l /= 100
+    v = l + s * Math.min(l, 1 - l)
+    if (v == 0) {
+        s = 0
+    } else {
+        2 * (1 - l / v)
     }
-    return c
+    return [Math.round(h), Math.round(s * 100), Math.round(v * 100)]
+}
+
+function rgbToHex(r, g, b) {
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1).toUpperCase();
+}
+
+function hexToRgv(hex) {
+    console.log(hex)
+    let bigint = parseInt(hex.slice(1), 16);
+    let r = (bigint >> 16) & 255;
+    let g = (bigint >> 8) & 255;
+    let b = bigint & 255;
+    console.log("good")
+    return [Math.round(r), Math.round(g), Math.round(b)]
 }
